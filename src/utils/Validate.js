6fdd6a58f559763076b16cfc6df78fcbcd153e97
adarsh311002 <adarsh.hit.aeie25@heritageit.edu.in>
2025-08-24 22:13:59 +0530

    password validation check update

--- src/utils/Validate.js
+++ src/utils/Validate.js
@@ -1,16 +1,12 @@
 
+export const Validate = (email, password) => {
+  const emailValidate = /^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/.test(email);
 
-export const Validate = (email,password) => {
- 
-    const emailValidate = /^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/.test(email)
-    const passwordValidate = /^.*(?=.{8,})(?=.*[a-zA-Z])(?=.*\d)(?=.*[!#$%&? "]).*$/.test(password)
+  if (!emailValidate) return "Email id is not valid";
 
-    if(!emailValidate) return "Email id is not valid"
-    if(!passwordValidate) return "Password not valid"
+  if (!password || password.length === 0) return "Password cannot be empty";
 
-    return null
+  return null;
+};
 
-
-  
-}
 
